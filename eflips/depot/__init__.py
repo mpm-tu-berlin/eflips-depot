e4501634d2431b55f64bc0875528739aae5cd10b
Ludger Heide <ludger.heide@tu-berlin.de>
2023-07-04 12:27:52 +0200

    Removed deprecated reference to GUI.

--- eflips/depot/__init__.py
+++ eflips/depot/__init__.py
@@ -6,7 +6,6 @@ __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 # 2nd: Importing of the eflips modules -> TODO Cleanups here to avoid all those imports
 
 
-import eflips.depot.gui
 import eflips.depot.layout_opt
 import eflips.depot.settings_config
 from eflips.depot.configuration import DepotConfigurator
