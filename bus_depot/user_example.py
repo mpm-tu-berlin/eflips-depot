85e4c982206b06825430381160e8e60832cfafea
shuyao <shuyao.guo@tu-berlin.de>
2023-09-12 15:27:02 +0200

    Added django settings

--- bus_depot/user_example.py
+++ bus_depot/user_example.py
@@ -3,6 +3,17 @@
 import os
 import depot.api.basic
 
+
+import djangosettings
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.djangosettings")
+import django
+from django.core import management
+
+django.setup()
+management.call_command("migrate")
+
+
 absolute_path = os.path.dirname(__file__)
 
 # File configuration
@@ -18,11 +29,17 @@ filename_template = os.path.join(
 )
 
 # Setup Simulation Host
+# TODO: replace with read from database
 host = depot.api.basic.init_simulation(
     filename_eflips_settings, filename_schedule, filename_template
 )
 
+# TODO: run ebustoolbox.tasks.run_ebus_toolbox() to get the input files for eflips
+# TODO: load eflips_input.json
+
+
 # Run simulation
+# TODO: run_simulation(data_from_database, data_from_simba)
 ev = depot.api.basic.run_simulation(host)
 
 # Generate input data for simBA
