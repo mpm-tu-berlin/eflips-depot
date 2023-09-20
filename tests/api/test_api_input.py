7c860ff7ce41ce59ea7563765105ffe358ad57d1
Ludger Heide <ludger.heide@tu-berlin.de>
2023-09-20 17:19:01 +0200

    Bugfixes in the test.

--- tests/api/test_api_input.py
+++ tests/api/test_api_input.py
@@ -1,10 +1,13 @@
+from depot.api import VehicleType
+
+
 class TestApiInput:
     def test_vehicle_type(self):
         """This method tests if a VehicleType object can be correctly successfully initialized and the curves are properly functioning"""
 
-        vehicle_type = APIVehicleType("SB_DC", 100, 15.0, None, 0.8, 0.2, 1.0)
+        vehicle_type = VehicleType("SB_DC", 100, 15.0, None, 0.8, 0.2, 1.0)
 
-        assert isinstance(vehicle_type, APIVehicleType)
+        assert isinstance(vehicle_type, VehicleType)
 
         # Test if charging curve can be initialized with a constant float
         assert isinstance(vehicle_type.charging_curve(0.5), float)
@@ -18,14 +21,14 @@ class TestApiInput:
         soc_list = [0, 0.8, 1]
         power_list = [0, 15, 0]
 
-        vehicle_type = APIVehicleType(
+        vehicle_type = VehicleType(
             "SB_DC", 100, (soc_list, power_list), None, 0.8, 0.2, 1.0
         )
         assert isinstance(vehicle_type.charging_curve(0.5), float)
 
         # Test if the charging curve can be successfully initialized with dict
         charging_curve_dict = {0.0: 0.0, 0.8: 15.0, 1.0: 0.0}
-        vehicle_type = APIVehicleType(
+        vehicle_type = VehicleType(
             "SB_DC", 100, charging_curve_dict, None, 0.8, 0.2, 1.0
         )
         assert isinstance(vehicle_type.charging_curve(0.5), float)
