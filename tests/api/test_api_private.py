d2e1433289ac06a4b141c7925fb914049db551e3
Ludger Heide <ludger.heide@tu-berlin.de>
2024-01-15 15:53:57 +0100

    Remove accidentally copy pasted code

--- tests/api/test_api_private.py
+++ tests/api/test_api_private.py
@@ -2,9 +2,7 @@ from datetime import datetime, timedelta, timezone
 
 import pytest
 import simpy
-import sqlalchemy
 from eflips.model import (
-    BatteryType,
     Event,
     EventType,
     VehicleType,
@@ -22,130 +20,6 @@ from eflips.depot.simple_vehicle import (
     VehicleType as EflipsVehicleType,
 )
 from eflips.depot.standalone import Timetable as EflipsTimeTable
-
-
-class TestVehicleType(TestHelpers):
-    def test_create_vehicle_type(self, session, scenario):
-        # Create a simple vehicle type, with only the required fields
-        vehicle_type = VehicleType(
-            name="Test Vehicle Type",
-            scenario=scenario,
-            battery_capacity=100,
-            charging_curve=[[0, 150], [1, 150]],
-            opportunity_charging_capable=True,
-        )
-        session.add(vehicle_type)
-        session.commit()
-
-        # Create one with all fields
-        battery_type = BatteryType(
-            scenario=scenario, specific_mass=100, chemistry={"test": "test"}
-        )
-        vehicle_type = VehicleType(
-            name="Test Vehicle Type",
-            scenario=scenario,
-            battery_type=battery_type,
-            battery_capacity=100,
-            battery_capacity_reserve=10,
-            charging_curve=[[0, 150], [1, 150]],
-            v2g_curve=[[0, 150], [1, 150]],
-            charging_efficiency=0.9,
-            opportunity_charging_capable=True,
-            minimum_charging_power=10,
-            length=10,
-            width=10,
-            height=10,
-            empty_mass=12000,
-        )
-        session.add(vehicle_type)
-        session.commit()
-
-    def test_create_vehicle_type_invalid_battery_capacity(self, scenario, session):
-        for battery_capacity in [-100, 0]:
-            with pytest.raises(sqlalchemy.exc.IntegrityError):
-                vehicle_type = VehicleType(
-                    name="Test Vehicle Type",
-                    scenario=scenario,
-                    battery_capacity=battery_capacity,
-                    charging_curve=[[0, 150], [1, 150]],
-                    opportunity_charging_capable=True,
-                )
-                session.add(vehicle_type)
-                session.commit()
-            session.rollback()
-
-    def test_create_vehicle_type_invalid_battery_capacity_reserve(
-        self, scenario, session
-    ):
-        with pytest.raises(sqlalchemy.exc.IntegrityError):
-            vehicle_type = VehicleType(
-                name="Test Vehicle Type",
-                scenario=scenario,
-                battery_capacity=100,
-                battery_capacity_reserve=-10,
-                charging_curve=[[0, 150], [1, 150]],
-                opportunity_charging_capable=True,
-            )
-            session.add(vehicle_type)
-            session.commit()
-        session.rollback()
-
-        # For reserve capacity, 0 is valid
-        vehicle_type = VehicleType(
-            name="Test Vehicle Type",
-            scenario=scenario,
-            battery_capacity=100,
-            battery_capacity_reserve=0,
-            charging_curve=[[0, 150], [1, 150]],
-            opportunity_charging_capable=True,
-        )
-        session.add(vehicle_type)
-        session.commit()
-
-    def test_create_vehicle_type_invalid_charging_efficiency(self, scenario, session):
-        for charging_efficiency in (-1, 0, 1.1):
-            with pytest.raises(sqlalchemy.exc.IntegrityError):
-                vehicle_type = VehicleType(
-                    name="Test Vehicle Type",
-                    scenario=scenario,
-                    battery_capacity=100,
-                    charging_curve=[[0, 150], [1, 150]],
-                    charging_efficiency=charging_efficiency,
-                    opportunity_charging_capable=True,
-                )
-                session.add(vehicle_type)
-                session.commit()
-            session.rollback()
-
-    def test_create_vehicle_type_invalid_minimum_charging_power(
-        self, scenario, session
-    ):
-        with pytest.raises(sqlalchemy.exc.IntegrityError):
-            vehicle_type = VehicleType(
-                name="Test Vehicle Type",
-                scenario=scenario,
-                battery_capacity=100,
-                charging_curve=[[0, 150], [1, 150]],
-                opportunity_charging_capable=True,
-                minimum_charging_power=-10,
-            )
-            session.add(vehicle_type)
-            session.commit()
-
-    def test_create_vehicle_type_invalid_empty_weight(self, scenario, session):
-        for empty_weight in (-100, 0):
-            with pytest.raises(sqlalchemy.exc.IntegrityError):
-                vehicle_type = VehicleType(
-                    name="Test Vehicle Type",
-                    scenario=scenario,
-                    battery_capacity=100,
-                    charging_curve=[[0, 150], [1, 150]],
-                    opportunity_charging_capable=True,
-                    empty_mass=empty_weight,
-                )
-                session.add(vehicle_type)
-                session.commit()
-            session.rollback()
 
 
 class TestVehicleType(TestHelpers):
