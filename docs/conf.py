d997bc6a42095e2a9ca75c3e0603d6223ae7c71f
Ludger Heide <ludger.heide@tu-berlin.de>
2024-02-07 17:01:38 +0100

    Bump version to 1.1.0 and update eflips-model dependency

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "eFLIPS-Depot"
 copyright = "2023, MPM TU Berlin"
 author = "MPM TU Berlin"
-release = "1.0.0"
+release = "1.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
