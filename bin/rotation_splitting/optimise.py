748403917501f39094fe70610536ea0481f5ecb5
Linus Seiffert <linusseiffert@gmail.com>
2024-10-15 17:04:58 +0200

    added loop to filter out obsolete oc-stations

--- bin/rotation_splitting/optimise.py
+++ bin/rotation_splitting/optimise.py
@@ -7,17 +7,21 @@ from toolkit import *
 station_standby = []
 station_blacklist = []  # stations that cannot be electrified (IDs)
 station_whitelist = []  # stations that have to be electrified (IDs)
+logger = logging.getLogger("custom")
+logging.basicConfig(level=logging.INFO, format='%(message)s', handlers=[logging.StreamHandler()])
 #############
 
 def run_optimisation():
     new_oc_stations = tk.run_single_step()
-    if len(new_oc_stations) > 1: print("\nError: Multiple Stations electrified, please implement case")
+    if len(new_oc_stations) > 1: logger.info("\nError: Multiple Stations electrified, please implement case")
 
     def split():
         tk.undo_electrification(new_oc_stations[0])
         tk.run_consumption_sim()
-        to_split = get_all_negative_rotations_at_station(tk, new_oc_stations[0])
-
+        to_split = get_negative_rotations_at_station(tk, new_oc_stations[0])
+        if len(to_split) == 0:
+            logger.info("\n###\tStation without negative rotations electrified. Please fix...")
+            tk.run_single_step()
         for rotation in to_split:
             # split_rotations.append(tk.get_rotation_info(rotation[0]))
             # split_rotation_ids.append(rotation[0])
@@ -32,19 +36,20 @@ def run_optimisation():
         for station in tk.eligible_stations:
             if station['station_id'] == new_oc_stations[0]:
                 eligible = True
-                print(f'\nElectrification of {stations[new_oc_stations[0]]} accepted.')
+                logger.info(f'\nElectrification of {stations[new_oc_stations[0]]} accepted.')
 
         if eligible: run_optimisation()
 
         if new_oc_stations[0] in station_blacklist:
-            print(f'\nElectrification of blacklisted station {stations[new_oc_stations[0]]} denied.')
+            logger.info(f'\nElectrification of blacklisted station {stations[new_oc_stations[0]]} denied.')
             split()
         elif not eligible:
-            print(f'\nElectrification of {stations[new_oc_stations[0]]} denied.')
+            logger.info(f'\nElectrification of {stations[new_oc_stations[0]]} denied.')
             split()
     else:
+        tk.remove_obsolete_stations()
         tk.run_depot_sim()
-        print(f"\nOptimization complete. Runtime: {round(time.time() - optimisation_start)} s."
+        logger.info(f"\nOptimization complete. Runtime: {round(time.time() - optimisation_start)} s."
               f"\n{len(tk.get_stations(electrified=True))} stations electrified."
               f"\n{tk.get_vehicle_count()} vehicles in scenario."
               f"\n{tk.get_rotation_count() - rotation_count} rotations split.")
@@ -53,10 +58,12 @@ def run_optimisation():
                   'stations': tk.get_stations(electrified=True),
                   'vehicles': tk.get_vehicle_count(),
                   'rotations': tk.get_rotation_count()}
-        print(result)
+        logger.info(result)
         tk.json_dump(result)
-        print("Optimisation result saved to results.json.")
+        logger.info("Optimisation result saved to results.json.")
 
+        # TODO add standby time from other depots to station_standby
+        #  select depot scenario stations first then calculate overall standby using all depot scenario
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -78,13 +85,13 @@ if __name__ == '__main__':
         args.database_url = os.environ["DATABASE_URL"]
 
     optimisation_start = time.time()
-    print('\nStarting optimization...')
+    logger.info('\nStarting optimization...')
     tk = Toolkit(args.database_url, args.scenario_id, args.percentile, True)
     # tk.run_depot_sim()
     tk.run_consumption_sim()
     tk = Toolkit(args.database_url, args.scenario_id, args.percentile)
     rotation_count = tk.get_rotation_count()
-    print(f'\n{len(tk.eligible_stations)} stations eligible for electrification'
+    logger.info(f'\n{len(tk.eligible_stations)} stations eligible for electrification'
           f' ({args.percentile} percentile):')
 
     run_optimisation()
