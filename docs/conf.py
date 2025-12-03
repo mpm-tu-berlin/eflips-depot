095b45a24c44330ed5a58422ce24c4f59334b184
Shuyao Guo <shuyao.guo@tu-berlin.de>
2025-12-03 14:40:12 +0100

    Version 4.15.6

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "eFLIPS-Depot"
 copyright = "2023, MPM TU Berlin"
 author = "MPM TU Berlin"
-release = "4.15.5"
+release = "4.15.6"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
